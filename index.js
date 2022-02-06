a859aa49a8cf3bf894980e03b06286b043c9e1fb
Zamal Hossain <thezamalian@gmail.com>
2022-02-06 11:28:14 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwjoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connected successfully!');
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hey Foodie Server!');  
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+const ourFoods = [
+    {
+        name: '',
+        img: '',
+        details: '',
+        price: '',
+        type: 'lunch',
+        typeCode: 2
+    }
+]
